f9e5fb6a51c3d8acd7080901fa2927462f750427
Maxim Lippeveld <maxim.lippeveld@ugent.be>
2020-05-03 09:43:15 +0200

    add style to all headings

--- theme/templatetags/tailwinds_filter.py
+++ theme/templatetags/tailwinds_filter.py
@@ -1,6 +1,7 @@
 from django import template
 from django.template.defaultfilters import stringfilter
 from bs4 import BeautifulSoup
+import re
 
 register = template.Library()
 
@@ -12,7 +13,7 @@ def tailwinds(value):
     for t in soup.find_all("p"):
         t["class"] = "mb-4"
     
-    for t in soup.find_all("h2"):
-        t["class"] = "my-2 font-semibold"
+    for t in soup.find_all(re.compile("h[123]")):
+        t["class"] = "my-2 font-semibold text-xl"
 
     return str(soup)
