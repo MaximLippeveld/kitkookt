48fe20d3bcaac2830a99960e1ca8798654232679
Maxim Lippeveld <maxim.lippeveld@ugent.be>
2020-08-27 20:16:27 +0200

    move critical values to dotenv

--- kitkookt/settings.py
+++ kitkookt/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dotenv
+dotenv.load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&^bpntp2h0y5me-)g7+n5@3e8eodhwx5k&v!asw3d*jdd0f45k'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ["DEBUG"] == 'true'
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", "kitkookt.be"]
 
