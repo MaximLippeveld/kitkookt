10386a8e86e4c0959a85a6ec0f68666b2ceb6f5d
Maxim Lippeveld <maxim.lippeveld@ugent.be>
2020-11-02 15:15:49 +0100

    Add leaf svg

--- kitkookt/settings.py
+++ kitkookt/settings.py
@@ -12,11 +12,11 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import dotenv
+import logging
 dotenv.load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -26,11 +26,10 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ["DEBUG"] == 'true'
+if DEBUG:
+    logging.getLogger(__name__).warning("DEBUG MODE")
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", "kitkookt.be"]
-
-COMPRESS_ENABLED = True
-
 
 # Application definition
 
