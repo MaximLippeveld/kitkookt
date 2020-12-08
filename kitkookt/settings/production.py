1559cf5fb7e36484923169407f2ebc5e730244d7
Maxim Lippeveld <maxim.lippeveld@ugent.be>
2020-12-08 09:01:15 +0100

    Gunicorn logging + docker run script

--- kitkookt/settings/production.py
+++ kitkookt/settings/production.py
@@ -2,4 +2,5 @@ from .base import *
 
 ALLOWED_HOSTS = ["0.0.0.0", "kitkookt.be"]
 SECRET_KEY = "_%!)ka%-khg!ckm6o$%2(0zcsn$@th%@gnm!q**@ozaq5*&((e"
-DEBUG = False
+DEBUG = True
+STATIC_URL = "http://localhost:9999/static/"
