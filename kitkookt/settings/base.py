519ffa11ec159bc350aff6e1dfd0b02e987de946
Maxim Lippeveld <lippeveld.maxim@gmail.com>
2021-01-17 12:36:48 +0100

    settings change

--- kitkookt/settings/base.py
+++ kitkookt/settings/base.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["kitkookt.be"]
 
 # Application definition
 
--- kitkookt/settings/local.py
+++ kitkookt/settings/local.py
@@ -1,5 +1,5 @@
 from .base import *
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "kitkookt.be"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 SECRET_KEY = "=kvqd+np85v+w$3@%+g+o356#h5%1(x+(mg$+l!4m@h^+yy0v="
 DEBUG = True
