ddb593c5d8377641eb3c71f1a953a26ba3fc8247
Maxim Lippeveld <maxim.lippeveld@ugent.be>
2021-10-24 12:36:40 +0200

    add fixed port

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/local/bin/gunicorn kitkookt.wsgi:application --bind "0.0.0.0:$PORT" --error-logfile error.log --access-logfile access.log --env DJANGO_SETTINGS_MODULE=kitkookt.settings.production
+/usr/local/bin/gunicorn kitkookt.wsgi:application --bind "0.0.0.0:7000" --error-logfile error.log --access-logfile access.log --env DJANGO_SETTINGS_MODULE=kitkookt.settings.production
